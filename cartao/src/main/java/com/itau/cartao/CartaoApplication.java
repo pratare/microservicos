8cd41aaa89538f6f8295ab1092a6b0843ef49177
Renato <renatoprata@gmail.com>
2020-03-11 22:09:56 -0300

    configurações microserviços

--- cartao/src/main/java/com/itau/cartao/CartaoApplication.java
+++ cartao/src/main/java/com/itau/cartao/CartaoApplication.java
@@ -2,8 +2,10 @@ package com.itau.cartao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CartaoApplication {
 
 	public static void main(String[] args) {
--- pagamento/src/main/java/com/itau/pagamento/PagamentoApplication.java
+++ pagamento/src/main/java/com/itau/pagamento/PagamentoApplication.java
@@ -2,8 +2,10 @@ package com.itau.pagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PagamentoApplication {
 
 	public static void main(String[] args) {
