40b7030932a154bf2c3c7f9e8d312f32fec901d6
Renato <renatoprata@gmail.com>
2020-03-11 21:59:14 -0300

    ajustes pagamentos

--- pagamento/src/main/java/com/itau/pagamento/dto/PagamentoMapper.java
+++ pagamento/src/main/java/com/itau/pagamento/dto/PagamentoMapper.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import com.itau.pagamento.dto.request.PagamentoRequest;
 import com.itau.pagamento.dto.response.PagamentoResponse;
 import com.itau.pagamento.dto.response.PagamentoSummaryInListResponse;
-import com.itau.pagamento.models.Cartao;
 import com.itau.pagamento.models.Pagamento;
 
 @Component
@@ -15,18 +14,15 @@ public class PagamentoMapper {
 		Pagamento pagamento = new Pagamento();
 		pagamento.setDescricao(pagamentoRequest.getDescricao());
 		pagamento.setValor(pagamentoRequest.getValor());
-		
-		Cartao cartao = new Cartao();
-		cartao.setId(pagamentoRequest.getCartaoid());
-		
-		pagamento.setCartao(cartao);
+		pagamento.setCartaoid(pagamentoRequest.getCartaoid());
 		
 		return pagamento;
 	}
 
 	public PagamentoResponse toPagamentoResponse(Pagamento pagamento) {
 		PagamentoResponse pagamentoResponse = new PagamentoResponse();
-		pagamentoResponse.setCartaoid(pagamento.getCartao().getId());
+		
+		pagamentoResponse.setCartaoid(pagamento.getCartaoid());
 		pagamentoResponse.setDescricao(pagamento.getDescricao());
 		pagamentoResponse.setValor(pagamento.getValor());
 		pagamentoResponse.setId(pagamento.getId());
@@ -40,7 +36,7 @@ public class PagamentoMapper {
         pagamentoSummaryInListResponse.setId(pagamento.getId());
         pagamentoSummaryInListResponse.setDescricao(pagamento.getDescricao());
         pagamentoSummaryInListResponse.setValor(pagamento.getValor());
-        pagamentoSummaryInListResponse.setCartaoId(pagamento.getCartao().getId());
+        pagamentoSummaryInListResponse.setCartaoId(pagamento.getCartaoid());
 
         return pagamentoSummaryInListResponse;
     }
