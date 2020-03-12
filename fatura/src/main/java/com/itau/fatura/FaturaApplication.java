00ce10cfe70109c2eea74db36e01d9ff4ae23752
Renato <renatoprata@gmail.com>
2020-03-11 22:16:40 -0300

    criando microservico fatura

---
+++ fatura/src/main/java/com/itau/fatura/FaturaApplication.java
@@ -0,0 +1,13 @@
+package com.itau.fatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturaApplication.class, args);
+	}
+
+}
